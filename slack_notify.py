ec629f9032d919d194f7428b451d478b0e82c326
Daniel2369 <120591150+Daniel2369@users.noreply.github.com>
2024-11-09 09:53:28 +0200

    Update slack_notify.py

--- slack_notify.py
+++ slack_notify.py
@@ -1,4 +1,4 @@
 # Send Slack Notification
     email = demisto.args().get('email')
     if email:
-        send_slack_notification = demisto.executeCommand("send-notification", {"message":readable_output,"to":email, "using":"SlackV2_Support"})
+        send_slack_notification = demisto.executeCommand("send-notification", {"message":readable_output,"to":email})
